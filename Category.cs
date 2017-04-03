b6f0fc5edd8956fc73a31fd31efdfb433fe6fca1
fathygeaiessah <fgeaiessah@gmail.com>
2017-04-03 21:41:05 +0200

    Add files via upload

---
+++ Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppInventory.Models
+{
+    public class Category
+    {
+        [Required]
+        [Display(Name = "م")]
+        public int ID { get; set; }
+        [Display(Name = "المجموعة")]
+        public string CategoryName { get; set; }
+        [Display(Name = "المجموعة الرئيسية")]
+        public string ParentCategory { get; set; }
+    }
+}
