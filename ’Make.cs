c1f5a1fb8bf0244ce4f324f1053040814fb5f2e6
fathygeaiessah <fgeaiessah@gmail.com>
2017-04-03 21:39:35 +0200

    Add files via upload

---
+++ ’Make.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppInventory.Models
+{
+    public class Make
+    {
+        [Required]
+        [Display(Name = "م")]
+        public int ID { get; set; }
+        [Display(Name = "الماركة")]
+        public string MakeName { get; set; }
+        [Display(Name = "المنشأ")]
+        public string Origin { get; set; }
+    }
+}
